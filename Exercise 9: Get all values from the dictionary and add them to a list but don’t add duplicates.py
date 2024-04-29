0932f75c4dfb18032fbf71788c58ef0975d51dca
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-04-29 11:41:19 +0700

    Solved 9

---
+++ Exercise 9: Get all values from the dictionary and add them to a list but don’t add duplicates.py
@@ -0,0 +1,25 @@
+# Exercise 9: Get all values from the dictionary and add them to a list but don’t add duplicates
+# Given:
+
+# speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+
+# Expected Outcome:
+
+# [47, 52, 44, 53, 54]
+def get_value(speed:dict):
+    valueList:list = list(dict.fromkeys(speed.values()))
+    print(f"The result: {valueList}")
+def main():
+    speed = {'jan': 47, 
+             'feb': 52, 
+             'march': 47, 
+             'April': 44, 
+             'May': 52, 
+             'June': 53, 
+             'july': 54, 
+             'Aug': 44, 
+             'Sept': 54}
+    
+    get_value(speed)
+if __name__=="__main__":
+    main()
